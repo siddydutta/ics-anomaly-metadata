1b2a841a5fffb08662eb5595b7011026e07e0214
Siddhartha Pratim Dutta <2897074D@student.gla.ac.uk>
2025-08-01 10:36:08 +0100

    Use LlamaIndex for Embeddings

--- persist_embeddings.py
+++ persist_embeddings.py
@@ -1,48 +1,75 @@
 import json
 import os
 
-import chromadb
-from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from dotenv import load_dotenv
+from llama_cloud import PipelineCreate, PipelineMetadataConfig
+from llama_cloud.client import LlamaCloud
+from llama_cloud.types import CloudDocumentCreate
+from llama_cloud.types.advanced_mode_transform_config_segmentation_config import (
+    AdvancedModeTransformConfigSegmentationConfig_Page,
+)
+from llama_cloud.types.open_ai_embedding import OpenAiEmbedding
+from llama_cloud.types.pipeline_create_embedding_config import (
+    PipelineCreateEmbeddingConfig_OpenaiEmbedding,
+)
+from llama_cloud.types.pipeline_create_transform_config import AutoTransformConfig
 from tqdm import tqdm
 
 load_dotenv()
 
+
+PIPELINE_NAME = "ICS Knowledge Base"
 DOCUMENTS_FILE = "documents.json"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
-client = chromadb.CloudClient(
-    api_key=os.getenv("CHROMADB_KEY"),
-    tenant=os.getenv("CHROMADB_TENANT"),
-    database=os.getenv("CHROMADB_DATABASE"),
-)
-
-collection = client.get_or_create_collection(
-    name="knowledge_base",
-    embedding_function=OpenAIEmbeddingFunction(
-        api_key=os.getenv("OPENAI_API_EMBEDDING_KEY"),
+EMBEDDING_CONFIG = PipelineCreateEmbeddingConfig_OpenaiEmbedding(
+    type="OPENAI_EMBEDDING",
+    component=OpenAiEmbedding(
+        api_key=os.getenv("OPENAI_API_KEY"),
         model_name=EMBEDDING_MODEL,
     ),
 )
 
-with open(DOCUMENTS_FILE, "r") as file:
-    documents = json.load(file)
+TRANSFORM_CONFIG = AutoTransformConfig(
+    segmentation_config=AdvancedModeTransformConfigSegmentationConfig_Page(mode="page"),
+    chunking_config=None,
+)
 
-ids = [doc["id"] for doc in documents]
-texts = [doc["text"] for doc in documents]
-metadatas = []
-for doc in documents:
-    metadata = doc.get("metadata", {})
-    # Remove keys with value None
-    cleaned_metadata = {k: v for k, v in metadata.items() if v is not None}
-    metadatas.append(cleaned_metadata)
+METADATA_CONFIG = PipelineMetadataConfig(
+    excluded_embed_metadata_keys=["pipeline_id", "document_id"],
+    excluded_llm_metadata_keys=["pipeline_id", "document_id"],
+)
+
+client = LlamaCloud(token=os.getenv("LLAMA_CLOUD_API_KEY"))
+
+pipeline_request = PipelineCreate(
+    name=PIPELINE_NAME,
+    embedding_config=EMBEDDING_CONFIG,
+    transform_config=TRANSFORM_CONFIG,
+    metadata_config=METADATA_CONFIG,
+)
+pipeline = client.pipelines.upsert_pipeline(request=pipeline_request)
+
+
+with open(DOCUMENTS_FILE, "r") as file:
+    raw_documents = json.load(file)
+
 
 BATCH_SIZE = 50
 for i in tqdm(
-    range(0, len(documents), BATCH_SIZE), desc="Persisting embeddings", unit="batch"
+    range(0, len(raw_documents), BATCH_SIZE), desc="Uploading documents", unit="batch"
 ):
-    collection.add(
-        ids=ids[i : i + BATCH_SIZE],
-        documents=texts[i : i + BATCH_SIZE],
-        metadatas=metadatas[i : i + BATCH_SIZE],
+    documents = []
+    for doc in raw_documents[i : i + BATCH_SIZE]:
+        cleaned_metadata = {
+            k: v for k, v in doc.get("metadata", {}).items() if v is not None
+        }
+        document = CloudDocumentCreate(
+            id=doc["id"],
+            text=doc["text"],
+            metadata=cleaned_metadata,
+        )
+        documents.append(document)
+    client.pipelines.create_batch_pipeline_documents(
+        pipeline_id=pipeline.id, request=documents
     )
